fa18f4a7b7eca0908ad1df3f545e486347c2af8b
gabrielhilal <gabrielhilal@gmail.com>
2013-01-23 17:03:35 +0000

    first commit

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,11 +1,16 @@
 class Staff < ActiveRecord::Base
-  attr_accessible :hours, :name, :total
+  attr_accessible :hours, :name, :total, :start, :finish
+  attr_accessor :start, :finish
+  before_save :set_hours
 
   validates :name,  presence: true
-  validates :hours, presence: true
 
   def self.total
     Staff.pluck(:hours).sum
+  end
+
+  def set_hours
+    self.hours = finish.to_f - start.to_f unless hours
   end
 
 end
